25cf610c47f250d52371e53264ee0404dda313ad
aoctaviopb <andresperez192@hotmail.com>
2023-03-06 12:44:56 -0500

    Reconocimiento de texto y tradiccion funcional. Hacer refactoring al codigo y organizar homeScreen.

--- screen_translator_1/lib/home_screen.dart
+++ screen_translator_1/lib/home_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:google_mlkit_translation/google_mlkit_translation.dart';
 import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,6 +14,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   File? image;
+  String texto = "Aca va texto";
+  String traduccion = "Aca va una traducción";
+  final textRecognizer = TextRecognizer(
+    script: TextRecognitionScript.korean,
+  );
 
   void pickImage() async {
     final ImagePicker imagePicker = ImagePicker();
@@ -19,6 +26,56 @@ class _HomeScreenState extends State<HomeScreen> {
         await imagePicker.pickImage(source: ImageSource.gallery);
     image = File(imgPick!.path);
     setState(() {});
+  }
+
+  void recognizeText() async {
+    final InputImage inputImage;
+    inputImage = InputImage.fromFile(image!);
+
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+    String text = recognizedText.text;
+    //print(text);
+    setState(() {
+      texto = text;
+    });
+
+    translateText(text);
+
+/*     for (TextBlock block in recognizedText.blocks) {
+      final String text = block.text;
+      final List<String> languages = block.recognizedLanguages;
+
+      for (TextLine line in block.lines) {
+        // Same getters as TextBlock
+        
+        for (TextElement element in line.elements) {
+          // Same getters as TextBlock
+        }
+      }
+    } */
+  }
+
+  void translateText(String text) async {
+    final TranslateLanguage sourceLanguage;
+    final TranslateLanguage targetLanguage;
+
+    final onDeviceTranslator = OnDeviceTranslator(
+      sourceLanguage: TranslateLanguage.korean,
+      targetLanguage: TranslateLanguage.english,
+    );
+
+    final String response = await onDeviceTranslator.translateText(text);
+    print(response);
+    setState(() {
+      traduccion = response;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    textRecognizer.close();
   }
 
   @override
@@ -41,20 +98,38 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             Expanded(
-              child: Container(
-                alignment: Alignment.center,
-                child: image == null
-                    ? const Text('Soy uma imagen',)
-                    : Image.file(
-                        image!,
-                      ),
+              child: InteractiveViewer(
+                child: Container(
+                  alignment: Alignment.center,
+                  child: image == null
+                      ? const Text(
+                          'Soy uma imagen',
+                        )
+                      : Image.file(
+                          image!,
+                        ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(texto),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(traduccion),
+                ],
               ),
             ),
             const SizedBox(
               height: 10,
             ),
             TextButton(
-              onPressed: () {},
+              onPressed: recognizeText,
               child: const Text('Traducir'),
             )
           ],
